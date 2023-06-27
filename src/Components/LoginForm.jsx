1362702cfd9d7de90eb48def423a70a6ad59158c
filiperslima <98400757+filiperslima@users.noreply.github.com>
2023-06-27 18:09:45 -0300

    atualizações no darkmode

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,9 +1,14 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ThemesContext } from "../Contextos/ThemesContext";
 import styles from "./Form.module.css";
 
 const LoginForm = () => {
+  
+  const { theme } = useContext(ThemesContext)
 
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     //Nesse handlesubmit você deverá usar o preventDefault,
     //enviar os dados do formulário e enviá-los no corpo da requisição 
     //para a rota da api que faz o login /auth
@@ -18,9 +23,9 @@ const LoginForm = () => {
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar o css correto */}
       <div
-        className={`text-center card container ${styles.card}`}
+        className={theme.body == "dark" ? `text-center card container ${styles.card} ${styles.cardDark} ` : `text-center card container ${styles.card}`}
       >
-        <div className={`card-body ${styles.CardBody}`}>
+        <div className={`card-body ${theme.card}`}>
           <form onSubmit={handleSubmit}>
             <input
               className={`form-control ${styles.inputSpacing}`}
