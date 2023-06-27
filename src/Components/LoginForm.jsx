388ad628ab6cdc4c7861fc8852c976cd0b6b603b
zillly <gil-hell@hotmail.com>
2023-06-27 14:33:26 -0300

    parte do login

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,48 +1,78 @@
 import styles from "./Form.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 
 const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://dhodonto.ctdprojetointegrador.com/auth', {
+        username,
+        password
+      });
+      const token = response.data.token;
+      localStorage.setItem("token", token);
   
-  const handleSubmit = (e) => {
-    //Nesse handlesubmit você deverá usar o preventDefault,
-    //enviar os dados do formulário e enviá-los no corpo da requisição 
-    //para a rota da api que faz o login /auth
-    //lembre-se que essa rota vai retornar um Bearer Token e o mesmo deve ser salvo
-    //no localstorage para ser usado em chamadas futuras
-    //Com tudo ocorrendo corretamente, o usuário deve ser redirecionado a página principal,com react-router
-    //Lembre-se de usar um alerta para dizer se foi bem sucedido ou ocorreu um erros
+      console.log("Login sucesso");
+    } catch (error) {
+      alert("Verifique suas informações novamente");
+    }
   };
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const validateForm = () => {
+    return username.length >= 5 && password.length >= 5;
+  };
+
+  useEffect(() => {
+    setIsButtonDisabled(!validateForm());
+  }, [username, password]);
+
   return (
-    <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
-      <div
-        className={`text-center card container ${styles.card}`}
-      >
-        <div className={`card-body ${styles.CardBody}`}>
-          <form onSubmit={handleSubmit}>
-            <input
-              className={`form-control ${styles.inputSpacing}`}
-              placeholder="Login"
-              name="login"
-              required
-            />
-            <input
-              className={`form-control ${styles.inputSpacing}`}
-              placeholder="Password"
-              name="password"
-              type="password"
-              required
-            />
-            <button className="btn btn-primary" type="submit">
-              Send
-            </button>
-          </form>
-        </div>
+    <div className={`text-center card container ${styles.card}`}>
+      <div className={`card-body ${styles.CardBody}`}>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={`form-control ${styles.inputSpacing}`}
+            placeholder="Login"
+            name="username"
+            value={username}
+            onChange={handleInputChange}
+            required
+          />
+          <input
+            className={`form-control ${styles.inputSpacing}`}
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={handleInputChange}
+            required
+          />
+          <button className="btn btn-primary" type="submit" disabled={isButtonDisabled} >
+            Send
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
 export default LoginForm;
+
