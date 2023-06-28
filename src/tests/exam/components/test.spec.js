4417520facaedd010e7458a68bf823f39327f4bc
zillly <gil-hell@hotmail.com>
2023-06-27 22:14:53 -0300

    arrumando teste

--- src/tests/exam/components/test.spec.js
+++ src/tests/exam/components/test.spec.js
@@ -1,7 +1,26 @@
-import { render, screen } from "../../test-utils"
-import Login from '../../../Routes/Login';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import LoginForm from "../../../Components/LoginForm";
 
-test('should show login form', () => {
-  render(<Login />)
-  expect(screen.getByText('Login')).toBeInTheDocument();
+describe("LoginForm", () => {
+  it("deve realizar o login com sucesso", async () => {
+    // Renderiza o componente LoginForm
+    render( <LoginForm/>);
+
+    // Obtém os elementos de input
+    const loginInput = screen.getByPlaceholderText("Login");
+    const passwordInput = screen.getByPlaceholderText("Password");
+
+    // Preenche os campos de input
+    fireEvent.change(loginInput, { target: { value: "dentistaAdmin" } });
+    fireEvent.change(passwordInput, { target: { value: "admin123" } });
+
+    // Simula o envio do formulário
+    fireEvent.click(screen.getByText("Send"));
+  
+    // Verifica se o token foi armazenado no localStorage
+    expect(localStorage.getItem("token")).not.toBeNull();
+
+
+  });
 });
