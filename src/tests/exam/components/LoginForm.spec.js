02cd3c7132236d9de74fd594c4ee975d70912358
filiperslima <98400757+filiperslima@users.noreply.github.com>
2023-07-01 18:44:53 -0300

    finalizado

--- src/tests/exam/components/LoginForm.spec.js
+++ src/tests/exam/components/LoginForm.spec.js
@@ -18,9 +18,27 @@ describe("LoginForm", () => {
   });
 
 
+  it("Deve liberar o botão e realizar o login", async () => {
+    // Renderiza o componente LoginForm
+    renderContext(<LoginForm />);
+
+    // Obtém os elementos de input
+    const loginInput = screen.getByPlaceholderText("Login");
+    const passwordInput = screen.getByPlaceholderText("Password");
+
+    // Preenche os campos de input
+    fireEvent.change(loginInput, { target: { value: "dentistaAdmin" } });
+    fireEvent.change(passwordInput, { target: { value: "admin123" } });
+
+    // Simula o envio do formulário
+    fireEvent.click(screen.getByText("Send"));
+
+  });
+
+
   it("Deve solicitar verificação das informações de login", async () => {
     // Renderiza o componente LoginForm
-    
+
     renderContext(<LoginForm />);
 
     // Obtém os elementos de input
@@ -36,6 +54,21 @@ describe("LoginForm", () => {
 
     await waitFor(() => { expect(mockAlert).toHaveBeenCalledWith(expect.stringContaining('Verifique suas informações novamente')) })
 
+  });
+  test('validação incorreta de login e senha', () => {
+    renderContext(<LoginForm />);
+
+    // Obter os elementos de input
+    const loginInput = screen.getByPlaceholderText('Login');
+    const passwordInput = screen.getByPlaceholderText('Password');
+
+    // Preencher os campos com valores inválidos
+    fireEvent.change(loginInput, { target: { value: 'user' } });
+    fireEvent.change(passwordInput, { target: { value: 'pass' } });
+
+    // Verificar se o botão de envio está desabilitado
+    const submitButton = screen.getByText('Send');
+    expect(submitButton).toBeDisabled();
   });
 });
 
